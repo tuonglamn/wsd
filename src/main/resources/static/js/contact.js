ed66f2f18239033ba35ec10e314e8c7315e5cd9a
tuonglamnguyen <tuonglamn@gmail.com>
2017-09-08 12:27:10 +0700

    new contact page

---
+++ src/main/resources/static/js/contact.js
@@ -0,0 +1,4 @@
+require(['app', 'map'],function(App, map){
+	App.init();
+	map.mapStart();
+});
--- src/main/resources/static/js/libs/map.js
+++ src/main/resources/static/js/libs/map.js
@@ -1,18 +1,45 @@
 define(['googlemaps!'],function(gmaps){
 	return {
 		mapStart: function(){
-			myCenter=new gmaps.LatLng(41.878114, -87.629798);
+			//center Washspeed Tân An
+			TanAn=new gmaps.LatLng(10.534907, 106.4087704);
+			//other Washspeed Bến Tre
+			BenTre=new gmaps.LatLng(10.2418703, 106.3745495);
 			  var mapOptions= {
-			    center:myCenter,
-			    zoom:12, scrollwheel: false, draggable: false,
+			    center: TanAn,
+			    zoom:9, scrollwheel: false, draggable: false,
 			    mapTypeId:gmaps.MapTypeId.ROADMAP
 			  };
 			  var map=new gmaps.Map(document.getElementById("googleMap"),mapOptions);
 
-			  var marker = new gmaps.Marker({
-			    position: myCenter,
+			  var markerTanAn = new gmaps.Marker({
+			    position: TanAn,
+			    label: "Washspeed Tân An",
+			    title: "Washspeed Tân An"
 			  });
-			  marker.setMap(map);
+			  markerTanAn.setMap(map);
+			  markerTanAn.addListener('click', function() {
+		          map.setZoom(16);
+		          map.setCenter(markerTanAn.getPosition());
+		          window.setTimeout(function() {
+		              map.setZoom(9);
+		            }, 3000);
+		        });
+
+			  var markerBenTre = new gmaps.Marker({
+				    position: BenTre,
+				    label: "Washspeed Bến Tre",
+				    title: "Washspeed Bến Tre"
+				  });
+			  markerBenTre.setMap(map);
+			  markerBenTre.addListener('click', function() {
+		          map.setZoom(16);
+		          map.setCenter(markerBenTre.getPosition());
+		          window.setTimeout(function() {
+		        	  map.setCenter(markerTanAn.getPosition());
+		              map.setZoom(9);
+		            }, 3000);
+		        });
 		}
 	}
 })
