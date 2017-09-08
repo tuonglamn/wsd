f7d55819d918e5f9c85f0b67a97d03f48ee03b57
tuonglamnguyen <tuonglamn@gmail.com>
2017-09-08 21:11:31 +0700

    add grid content

--- src/main/resources/static/js/libs/map.js
+++ src/main/resources/static/js/libs/map.js
@@ -1,47 +1,53 @@
-define(['googlemaps!'],function(gmaps){
+define(['googlemaps!', 'lodash'],function(gmaps, _){
+	
+	function newPost(x, y){
+		return new gmaps.LatLng(x,y );
+	}
+	
+	function newMarker(map, post, label, title){
+		marker = new gmaps.Marker({
+		    position: post,
+		    label: label,
+		    title: title
+		  }); 
+		marker.setMap(map);
+		return marker;
+	}
+	
 	return {
-		mapStart: function(){
-			//center Washspeed Tân An
-			TanAn=new gmaps.LatLng(10.534907, 106.4087704);
-			//other Washspeed Bến Tre
-			BenTre=new gmaps.LatLng(10.2418703, 106.3745495);
+		mapStart: function() {
+			  //center Washspeed Tân An
+			  var TanAn = newPost(10.534907, 106.4087704);
+			  //other Washspeed Bến Tre
+			  var BenTre = newPost(10.2418703, 106.3745495);
+			  //center map
+			  var centerPos = TanAn;
 			  var mapOptions= {
-			    center: TanAn,
-			    zoom:9, scrollwheel: false, draggable: false,
-			    mapTypeId:gmaps.MapTypeId.ROADMAP
-			  };
-			  var map=new gmaps.Map(document.getElementById("googleMap"),mapOptions);
-
-			  var markerTanAn = new gmaps.Marker({
-			    position: TanAn,
-			    label: "Washspeed Tân An",
-			    title: "Washspeed Tân An"
-			  });
-			  markerTanAn.setMap(map);
+			    center: TanAn,	  
+				zoom:9, scrollwheel: false, draggable: false,
+				mapTypeId:gmaps.MapTypeId.ROADMAP
+		      };
+			  var map = new gmaps.Map(document.getElementById("googleMap"), mapOptions);
+			  var markerTanAn = newMarker(map, TanAn, "Washspeed Tân An", "Washspeed Tân An"); 
 			  markerTanAn.addListener('click', function() {
-		          map.setZoom(16);
-		          map.setCenter(markerTanAn.getPosition());
-		          window.setTimeout(function() {
-		              map.setZoom(9);
-		            }, 3000);
-		        });
-
-			  var markerBenTre = new gmaps.Marker({
-				    position: BenTre,
-				    label: "Washspeed Bến Tre",
-				    title: "Washspeed Bến Tre"
-				  });
-			  markerBenTre.setMap(map);
+			          map.setZoom(16);
+			          map.setCenter(markerTanAn.getPosition());
+			          window.setTimeout(function() {
+			        	  map.setCenter(centerPos);
+			              map.setZoom(9);
+			            }, 3000);
+			        });
+			  var markerBenTre = newMarker(map, BenTre, "Washspeed Bến Tre", "Washspeed Bến Tre");
 			  markerBenTre.addListener('click', function() {
 		          map.setZoom(16);
 		          map.setCenter(markerBenTre.getPosition());
 		          window.setTimeout(function() {
-		        	  map.setCenter(markerTanAn.getPosition());
+		        	  map.setCenter(centerPos);
 		              map.setZoom(9);
 		            }, 3000);
 		        });
+			}
 		}
-	}
 })
 
 
